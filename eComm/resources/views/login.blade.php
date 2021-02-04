35348f74025b0fced070a97b5cc2208b35510630
jelenarankovic <jrankovic13@gmail.com>
2021-02-04 21:58:45 +0100

    dodata forma

--- eComm/resources/views/login.blade.php
+++ eComm/resources/views/login.blade.php
@@ -1,3 +1,22 @@
 @extends('master')
 @section("content")
-<h1>Login page</h1>
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4" >
+            <form >
+                <div class="form-group">
+                    <label for="exampleInputEmail1">E-mail</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Lozinka</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Lozinka">
+                </div>
+                <br>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
