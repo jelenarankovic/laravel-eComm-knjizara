cde5bb863e8271014ebc15366e85fb501c1b752d
jelenarankovic <jrankovic13@gmail.com>
2021-04-11 19:52:55 +0200

    problem sa kontrolorima i ne radi dobro logovanje, izbacuje gresku

--- eComm/resources/views/login.blade.php
+++ eComm/resources/views/login.blade.php
@@ -3,18 +3,19 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4" >
-            <form >
+            <form action="login" method ="POST">
                 <div class="form-group">
+                    @csrf
                     <label for="exampleInputEmail1">E-mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    <input type="email" name ="email"class="form-control" id="exampleInputEmail1" placeholder="Email">
                 </div>
                 <br>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Lozinka</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Lozinka">
+                    <input type="password" name = "password"class="form-control" id="exampleInputPassword1" placeholder="Lozinka">
                 </div>
                 <br>
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-default">Login</button>
             </form>
         </div>
     </div>
