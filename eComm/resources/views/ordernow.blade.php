48211051eb5dc6d258cff7f195933bd48a0ed1c7
jelenarankovic <jrankovic13@gmail.com>
2021-04-15 21:10:22 +0200

    ne radi dobro porucivanje u bazi

--- eComm/resources/views/ordernow.blade.php
+++ eComm/resources/views/ordernow.blade.php
@@ -19,15 +19,16 @@
             </tbody>
           </table>
           <div>
-            <form action="/action_page.php">
+            <form action="/orderplace" method="POST">
+              @csrf
                 <div class="form-group">
-                  <textarea type="email" placeholder="Unesite svoju adresu"class="form-control" id="email"></textarea>
+                  <textarea name="address" placeholder="Unesite svoju adresu"class="form-control" id="email"></textarea>
                 </div>
                 <div class="form-group">
                   <label for="pwd">Nacin placanja</label><br>
-                  <input type="radio" name="payment">Placanje platnom karticom<span></span><br><br>
-                  <input type="radio" name="payment">Placanje po uzecu<span></span> <br><br>
-                  <input type="radio" name="payment">Placanje uplatnicom<span></span><br><br>
+                  <input type="radio" value="card" name="payment">Placanje platnom karticom<span></span><br><br>
+                  <input type="radio" value="cash" name="payment">Placanje po uzecu<span></span> <br><br>
+                  <input type="radio" value="paper" name="payment">Placanje uplatnicom<span></span><br><br>
                 </div>
                 <button type="submit" class="btn btn-default">Poruci sad</button>
               </form>
